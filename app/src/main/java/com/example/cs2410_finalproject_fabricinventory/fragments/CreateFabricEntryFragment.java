76afde011e7e4b8a462fd759e22d0a4c77b4c983
Kaitlin Murphy <kaitlin.murphy@ccsdut.org>
2021-08-11 09:03:36 -0600

    Fixed text fields in create new fabric entry Started getting text from the fields when save button is clicked

--- app/src/main/java/com/example/cs2410_finalproject_fabricinventory/fragments/CreateFabricEntryFragment.java
+++ app/src/main/java/com/example/cs2410_finalproject_fabricinventory/fragments/CreateFabricEntryFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.cs2410_finalproject_fabricinventory.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CreateFabricEntryFragment extends Fragment {
 
@@ -17,6 +18,14 @@ public class CreateFabricEntryFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.save_button).setOnClickListener(saveButton -> {
+            TextInputEditText fabricNameText = view.findViewById(R.id.fabric_name_text);
+            TextInputEditText fabricLineNameText = view.findViewById(R.id.fabric_line_name_text);
+            TextInputEditText fabricAmountText = view.findViewById(R.id.fabric_amount_text);
+            TextInputEditText fabricPriceText = view.findViewById(R.id.fabric_price_text);
+            TextInputEditText fabricStorePurchasedAtText = view.findViewById(R.id.fabric_store_purchased_text);
+            TextInputEditText fabricAdditionalNotesText = view.findViewById(R.id.fabric_add_notes_text);
+        });
     }
 
 }
