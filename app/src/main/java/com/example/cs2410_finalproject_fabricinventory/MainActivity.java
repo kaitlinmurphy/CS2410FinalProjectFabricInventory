c2c021ec2a134bb998cbc3f94d45ac07bafb70fe
Kaitlin Murphy <kaitlin.murphy@ccsdut.org>
2021-08-11 00:47:12 -0600

    Blank android project

---
+++ app/src/main/java/com/example/cs2410_finalproject_fabricinventory/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs2410_finalproject_fabricinventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
