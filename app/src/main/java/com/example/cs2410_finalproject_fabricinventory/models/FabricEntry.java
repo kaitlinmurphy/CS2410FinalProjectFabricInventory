aa23d944a55d5768b4f29df80d830af14d287b25
Kaitlin Murphy <kaitlin.murphy@ccsdut.org>
2021-08-11 02:32:05 -0600

    Added FabricEntry model

---
+++ app/src/main/java/com/example/cs2410_finalproject_fabricinventory/models/FabricEntry.java
@@ -0,0 +1,11 @@
+package com.example.cs2410_finalproject_fabricinventory.models;
+
+public class FabricEntry {
+    String fabricName;
+    String fabricLineName;
+    double amount; // in yards
+    double price; // per yard
+    String storePurchasedAt;
+    String additionalNotes;
+    //Image???
+}
